5eeb3ee57ac3817478ed7cb08a4307ee9e50423d
Miguel Pereira <miguel.s.pereira@outlook.com>
2022-09-03 20:34:54 +0100

    init

---
+++ responsive-menu.js
@@ -0,0 +1,13 @@
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.nav-menu');
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('active');
+    navMenu.classList.toggle('active');
+})
+
+document.querySelectorAll('.nav-link').forEach(e => e.
+    addEventListener('click', () => {
+        hamburger.classList.remove('active');
+        navMenu.classList.remove('active');
+    }))
